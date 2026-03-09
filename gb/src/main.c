013daf3aa183fe829d37f231280d062885f4c6fc
Max Fierke <max@maxfierke.com>
2026-03-08 23:05:04 -0500

    gb: move palettes out to their own header

--- gb/src/main.c
+++ gb/src/main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 
 #include "link.h"
+#include "palettes.h"
 #include "term.h"
 
 // Keys
@@ -30,78 +31,6 @@ char line_buffer[20];
 
 // Drawing & Themes
 font_t title_font, term_console_font, invert_min_font;
-#define RGB_GOLD 0x331C
-#define RGB_DARK_PURPLE 0x2405
-#define RGB_DARK_GREEN 0x0923
-#define RGB_DARK_BLUE 0x3860
-#define RGB_DARK_ORANGE 0x012E
-#define RGB_DARK_RED 0x080E
-#define RGB_CERULEAN 0x5CC2
-#define RGB_ICE_BLUE 0x5E89
-#define RGB_GREY 0x3DEF
-#define RGB_MUTED_PINK 0x5573
-#define RGB_VIOLET 0x584C
-#define RGB_POCKET_WHITE 0x5BDB
-#define RGB_POCKET_LIGHT_GREY 0x4B15
-#define RGB_POCKET_DARK_GREY 0x3E4F
-#define RGB_POCKET_BLACK 0x2D89
-#define RGB_EXTREME_RED 0x0CB7
-#define RGB_LEAF_GREEN 0x2240
-#define RGB_DUSK_GREEN 0x2392
-#define RGB_DUSK_TEAL 0x4608
-#define RGB_DUSK_PURPLE 0x28A6
-#define RGB_SUNRISE_PURPLE 0x2CEF
-#define RGB_SUNRISE_ORANGE 0x3ABF
-#define RGB_SUNRISE_RED 0x35BD
-#define RGB_SUNRISE_YELLOW 0x6BDF
-
-// NORMAL TEXT, HIGHLIGHT TEXT, HIGHLIGHT/TITLE, BG
-const palette_color_t palettePurple[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                         RGB_DARK_PURPLE};
-const palette_color_t paletteBlue[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                       RGB_DARK_BLUE};
-const palette_color_t paletteGreen[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                        RGB_DARK_GREEN};
-const palette_color_t paletteOrange[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                         RGB_DARK_ORANGE};
-const palette_color_t paletteRed[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                      RGB_DARK_RED};
-const palette_color_t paletteBlack[] = {RGB_WHITE, RGB_BLACK, RGB_GOLD,
-                                        RGB_BLACK};
-const palette_color_t paletteSuperBlue[] = {RGB_WHITE, RGB_WHITE, RGB_ICE_BLUE,
-                                            RGB_CERULEAN};
-const palette_color_t paletteVeryGreen[] = {RGB_WHITE, RGB_GREEN,
-                                            RGB_DARK_GREEN, RGB_LEAF_GREEN};
-const palette_color_t paletteReallyPink[] = {RGB_WHITE, RGB_BLACK, RGB_VIOLET,
-                                             RGB_MUTED_PINK};
-const palette_color_t paletteExtremeRed[] = {RGB_WHITE, RGB_RED, RGB_BLACK,
-                                             RGB_EXTREME_RED};
-const palette_color_t paletteTerminal[] = {RGB_GREEN, RGB_WHITE, RGB_GREY,
-                                           RGB_BLACK};
-const palette_color_t paletteDotMatrix[] = {
-    RGB_POCKET_WHITE, RGB_POCKET_LIGHT_GREY, RGB_POCKET_DARK_GREY,
-    RGB_POCKET_BLACK};
-const palette_color_t paletteDusk[] = {RGB_WHITE, RGB_DUSK_GREEN, RGB_DUSK_TEAL,
-                                       RGB_DUSK_PURPLE};
-const palette_color_t paletteSunrise[] = {RGB_SUNRISE_PURPLE,
-                                          RGB_SUNRISE_ORANGE, RGB_SUNRISE_RED,
-                                          RGB_SUNRISE_YELLOW};
-const palette_color_t paletteWhiteHC[] = {RGB_BLACK, RGB_WHITE, RGB_BLACK,
-                                          RGB_WHITE};
-const palette_color_t paletteBlackHC[] = {RGB_WHITE, RGB_BLACK, RGB_WHITE,
-                                          RGB_BLACK};
-
-const char* themeNames[] = {
-    "     Purple", "       Blue", "      Green", "     Orange",
-    "        Red", "      Black", " Super Blue", " Very Green",
-    "Really Pink", "Extreme Red", "   Terminal", " Dot Matrix",
-    "       Dusk", "    Sunrise", "   White HC", "   Black HC"};
-palette_color_t* palettes[] = {
-    palettePurple,     paletteBlue,       paletteGreen,     paletteOrange,
-    paletteRed,        paletteBlack,      paletteSuperBlue, paletteVeryGreen,
-    paletteReallyPink, paletteExtremeRed, paletteTerminal,  paletteDotMatrix,
-    paletteDusk,       paletteSunrise,    paletteWhiteHC,   paletteBlackHC};
-const uint8_t themeCount = sizeof palettes / sizeof palettes[0];
 
 void setup_fonts(void) {
   font_init();
