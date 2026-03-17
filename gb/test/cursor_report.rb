0ff328ea72c0c62fa084d4a31b7185de874047d3
Max Fierke <max@maxfierke.com>
2026-03-16 21:57:19 -0500

    gb: update test script to write out test content prior to cursor reporting

--- gb/test/cursor_report.rb
+++ gb/test/cursor_report.rb
@@ -25,6 +25,38 @@ SerialPort.open(gbtty_path, baud_rate, 8, 1, SerialPort::NONE) do |sp|
 
   puts
 
+  sp.write("\e[1;1H\e[J\e[0m> GBTTY. Connected")
+  sp.flush
+  sleep 2
+
+  sp.write("\e[4;2HNormal text.")
+  sp.flush
+  sleep 2
+
+  sp.write("\e[6;2H\e[7mInverted text.\e[27m")
+  sp.flush
+  sleep 2
+
+  sp.write("\e[12;2HWaiting...")
+  sp.flush
+
+  32.times do |i|
+    sp.write("\e[8;2HLooped \e[7m#{i}\e[0m times")
+    sp.write("\e[12;12H")
+    sp.flush
+    sleep 0.5
+  end
+
+  sleep 0.2
+
+  sp.write("\e[12;2H\e[2KDone.")
+  sp.flush
+
+  sleep 0.2
+
+  # Discard some bytes we don't care about
+  sp.read(100)
+
   puts "Sending cursor position query (CSI 6n)..."
   sp.write("\e[6n")
   sp.flush
